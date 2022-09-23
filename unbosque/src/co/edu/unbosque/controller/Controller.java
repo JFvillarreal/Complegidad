540581351de24454f0ce12af5814672febe74b4d
Juan Sebastian Muñoz <sebastian.jsmopu@gmail.com>
2022-09-22 20:41:32 -0500

    Avances.

---
+++ unbosque/src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,35 @@
+package co.edu.unbosque.controller;
+import co.edu.unbosque.model.ClaseY;
+import co.edu.unbosque.view.View;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Controller {
+	
+
+	private ClaseY m;
+	private View gui;
+	
+	public Controller() {
+
+
+		
+		m = new ClaseY();
+		gui = new View(this);
+
+		gui.setVisible(true);
+		
+	}
+	
+	public void actionPerformed(ActionEvent evento) {
+		// TODO Auto-generated method stub
+		if (evento.getActionCommand().equals(gui.getPanelEntrada().CONVERTIR)) {
+			//String resultado = m.procesarDato(gui.getPanelEntrada().getTxtNumero().getText());
+			String resultado1 = m.getY().procesarDatoClaseY(gui.getPanelEntrada().getTxtNumero().getText());
+			gui.getPanelResultados().getTxtMonto().setText(resultado1);
+		}
+	}
+	
+}
