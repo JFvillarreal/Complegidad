ba994ebcf96ed8a35232d9c671d3cadaff5f6571
Javier Villarreal <jafevc@gmail.com>
2022-09-24 19:49:11 -0700

    algoritmo KMP

---
+++ unbosque/src/co/edu/unbosque/model/AlgoritmoKMP.java
@@ -0,0 +1,46 @@
+package co.edu.unbosque.model;
+import co.edu.unbosque.view.VistaVentanas;
+public class AlgoritmoKMP {
+
+	private static VistaVentanas vista;
+	
+	public static void KMP(String texto , String patron) {
+		
+		if (patron == null || patron.length() == 0) {
+			vista.mostrarInformacionError("error en patron");
+			return;
+		}
+		if(texto == null || patron.length()> texto.length()) {
+			vista.mostrarInformacionError("error en el texto");
+		}
+		
+		char[] chars = patron.toCharArray();
+		int[] next = new int[patron.length() +1];
+		for (int i = 1; i<patron.length(); i++) {
+			int j = next[i + 1];
+			while (j>0 && chars[j] != chars[i]) {
+				j = next[j];
+			}
+			if (j>0 || chars[j] == chars[i]) {
+				next[i+1] = j + 1;
+			}
+		}
+		for (int i = 0,j =0; i < texto.length(); i++) {
+			if (j < patron.length() && texto.charAt(i)==patron.charAt(j)) {
+				if (++j == patron.length()) {
+					System.out.println("patrones en " + (i-j+1));
+				}
+			}
+			else if (j>0)
+			{
+				j = next[j];
+				i--;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		String text = "adadadadas";
+		String part = "das";
+		KMP(text, part);
+	}
+}
