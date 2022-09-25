d668b3bb90d6000d7c3256cdc487a360b5de0494
Juan Sebastian Muñoz <sebastian.jsmopu@gmail.com>
2022-09-24 22:01:40 -0500

    Arreglo.

--- unbosque/src/co/edu/unbosque/controller/Controller.java
+++ unbosque/src/co/edu/unbosque/controller/Controller.java
@@ -1,46 +1,31 @@
 package co.edu.unbosque.controller;
-<<<<<<< HEAD
-=======
-import co.edu.unbosque.model.ClaseY;
-import co.edu.unbosque.model.LeerArchivo;
-import co.edu.unbosque.view.View;
->>>>>>> branch 'main' of https://github.com/JFvillarreal/Complegidad
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import co.edu.unbosque.model.LeerArchivo;
 import co.edu.unbosque.model.Numero;
-import co.edu.unbosque.view.Mensaje;
+
 import co.edu.unbosque.view.VentanaPrincipal;
 
 public class Controller implements ActionListener{
 	
-<<<<<<< HEAD
+
 	private VentanaPrincipal ventana;
 	private Numero n1;
 	private Numero n2;
-=======
 
-	private ClaseY m;
-	private View gui;
+
 	private LeerArchivo le;
->>>>>>> branch 'main' of https://github.com/JFvillarreal/Complegidad
-	
+
 	public Controller() {
-<<<<<<< HEAD
+
 		ventana = new VentanaPrincipal();
 		asignarOyentes();
-=======
 
 
-		
-		m = new ClaseY();
-		gui = new View(this);
-
-		gui.setVisible(true);
 		le.leer();
 		
->>>>>>> branch 'main' of https://github.com/JFvillarreal/Complegidad
 	}
 	
 	public void asignarOyentes() {
