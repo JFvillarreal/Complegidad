52dfd6854faa1275ce8f766ce02502401d757f25
EstebanHdez1 <91765395+EstebanHdez1@users.noreply.github.com>
2022-09-28 22:43:53 -0500

    a

---
+++ unbosque/src/co/edu/unbosque/view/VistaVentanas.java
@@ -0,0 +1,47 @@
+package co.edu.unbosque.view;
+
+import javax.swing.JOptionPane;
+/**
+ * sirve para mostrar JOptionpane con mensajes de error o 
+ * sobre los resusltados de los algoritmos
+ * @author Villarreal, Muños, Hernadez
+ *
+ */
+public class VistaVentanas {
+	
+	public VistaVentanas() {
+		
+	}
+	
+	public int leerDatoEntero(String mensaje) {
+		String aux = JOptionPane.showInputDialog(mensaje);
+		int dato = Integer.parseInt(aux);
+		return dato;
+	}
+	
+	public long leerDatoLong(String mensaje) {
+		String aux = JOptionPane.showInputDialog(mensaje);
+		long dato = Long.parseLong(aux);
+		return dato;
+	}
+	
+	public String leerDatoString(String mensaje) {
+		String dato = JOptionPane.showInputDialog(mensaje);
+		return dato;
+	}
+	
+	public double leerDatoReal(String mensaje) {
+		String aux = JOptionPane.showInputDialog(mensaje);
+		double dato = Double.parseDouble(aux);
+		return dato;
+	}
+	
+	public void mostrarInformacion(String mensaje) {
+		JOptionPane.showMessageDialog(null, mensaje);
+	}
+	
+	public void mostrarInformacionError(String mensaje) {
+		JOptionPane.showMessageDialog(null, mensaje, "ERROR", JOptionPane.ERROR_MESSAGE);
+	}
+
+}
